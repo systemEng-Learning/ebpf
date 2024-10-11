b2ee362ef5ff4cc3bfc03437df7a3efa3bfdebf6
Goodness Duru <goodyduru@gmail.com>
2024-10-11 17:41:35 +0100

    Trace tcp_v4_rcv and tcp_rcv_state_process

--- python/track.c
+++ python/track.c
@@ -1,11 +1,13 @@
 #include <uapi/linux/ptrace.h>
 #include <linux/tcp.h>
+#include <linux/ip.h>
 #include <net/sock.h>
+#include <bcc/proto.h>
 
 
-BPF_HASH(traced, struct sock *, u8);
-BPF_HASH(counts, struct sock *, u64);
-BPF_HASH(is_rst, struct sock *, u8);
+BPF_HASH(syn_map, u32, u8);
+BPF_HASH(ack_map, u32, u8);
+BPF_HASH(data_map, struct sock *, u64);
 BPF_HISTOGRAM(dist);
 
 static struct tcphdr *skb_to_tcphdr(const struct sk_buff *skb) {
@@ -13,7 +15,77 @@ static struct tcphdr *skb_to_tcphdr(const struct sk_buff *skb) {
 	return (struct tcphdr *)(skb->head + skb->transport_header);
 }
 
-int kprobe__tcp_set_state(struct pt_regs *ctx, struct sock *sk, int state) {
+static inline struct iphdr *skb_to_iphdr(const struct sk_buff *skb) {
+    return (struct iphdr *)(skb->head + skb->network_header);
+}
+
+int kprobe__tcp_v4_rcv(struct pt_regs *ctx, struct sk_buff *skb) {
+    struct iphdr *ip = skb_to_iphdr(skb);
+    struct tcphdr *tcp = skb_to_tcphdr(skb);
+    u8 flag = ((u_int8_t *)tcp)[13];
+    u8 rst = flag & 4;
+    u32 key = ip->saddr + (u32)tcp->source;
+    u8 *synp = syn_map.lookup(&key);
+    if ( rst && synp != 0 ) {
+        syn_map.delete(&key);
+        bpf_trace_printk("state: rst_received: %u\\n", key);
+        dist.increment(0);
+    }
+    return 0;
+}
+
+
+int kprobe__tcp_rcv_state_process(struct pt_regs *ctx, struct sock *sk, struct sk_buff *skb) {
+    if ( sk->sk_state != TCP_SYN_RECV && sk->sk_state != TCP_LISTEN  ) {
+        return 0;
+    }
+    u16 family = sk->__sk_common.skc_family;
+    if ( family != AF_INET ) {
+        return 0;
+    }
+    struct iphdr *ip = skb_to_iphdr(skb);
+    struct tcphdr *tcp = skb_to_tcphdr(skb);
+    u8 flag = ((u_int8_t *)tcp)[13];
+    u8 fin = flag & 1;
+    u8 syn = flag & 2;
+    u8 ack = flag & 16;
+    u32 key = ip->saddr + (u32)tcp->source;
+    u8 *synp, *ackp;
+    if ( fin ) {
+        synp = syn_map.lookup(&key);
+        ackp = ack_map.lookup(&key);
+        if ( synp != 0 ) {
+            syn_map.delete(&key);
+        }
+        if ( ackp != 0 ) {
+            ack_map.delete(&key);
+        }
+        return 0;
+    }
+    u8 ts = 1;
+
+    if ( syn && !fin ) {
+        syn_map.update(&key, &ts);
+        bpf_trace_printk("state: syn_received: %u\\n", key);
+    }
+
+    synp = syn_map.lookup(&key);
+    if ( ack && !fin && synp != 0 ) {
+        syn_map.delete(&key);
+        ack_map.update(&key, &ts);
+        bpf_trace_printk("state: ack_received: %u\\n", key);
+    }
+    return 0;
+}
+
+int kprobe__tcp_rcv_established(struct pt_regs *ctx, struct sock *sk, struct sk_buff *skb) {
+    if ( sk->sk_state == TCP_SYN_RECV ) {
+        bpf_trace_printk("2current_state: received");
+    }
+    return 0;
+}
+
+/**int kprobe__tcp_set_state(struct pt_regs *ctx, struct sock *sk, int state) {
     u8 *tsp, ts = 1;
     u64 *csp;
     tsp = traced.lookup(&sk);
@@ -64,23 +136,6 @@ int kprobe__tcp_set_state(struct pt_regs *ctx, struct sock *sk, int state) {
     return 0;
 }
 
-int kprobe__tcp_rcv_established(struct pt_regs *ctx, struct sock *sk, struct sk_buff *skb) {
-    u8 *tsp;
-    tsp = traced.lookup(&sk);
-    if ( tsp == 0 ) {
-        return 0;
-    }
-    u64 *csp;
-    csp = counts.lookup(&sk);
-    if ( csp == 0 ) {
-        u64 c = 1;
-        counts.update(&sk, &c);
-    } else {
-        counts.increment(sk);
-    }
-    return 0;
-}
-
 int kprobe__tcp_reset(struct pt_regs *ctx, struct sock *sk, struct sk_buff *skb) {
     u8 *tsp, ts = 1;
     tsp = traced.lookup(&sk);
@@ -108,20 +163,5 @@ int kprobe__tcp_reset(struct pt_regs *ctx, struct sock *sk, struct sk_buff *skb)
     }
     is_rst.update(&sk, &ts);
     return 0;
-}
+}*/
 
-int kprobe__tcp_rcv_state_process(struct pt_regs *ctx, struct sock *sk, struct sk_buff *skb) {
-    if ( sk->__sk_common.skc_state != TCP_LISTEN ) {
-        return 0;
-    }
-    struct tcphdr *tcp = skb_to_tcphdr(skb);
-    u8 flag = ((u_int8_t *)tcp)[13];
-    u8 syn = flag & 2;
-    u8 fin = flag & 1;
-    u8 ts = 1;
-    if ( syn && !fin ) {
-        traced.update(&sk, &ts);
-        bpf_trace_printk("state: syn_received\\n");
-    }
-    return 0;
-}
